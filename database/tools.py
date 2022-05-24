c429b16c7bada151f5ba3ef8bf0eeae997137e52
discovery-bit <61625792+discovery-bit@users.noreply.github.com>
2022-05-24 21:36:48 +0200

    added: get_contacts        add_contact        remove_contact        get_messages        add_message        change_password
    functions to interact with the database

--- database/tools.py
+++ database/tools.py
@@ -2,10 +2,11 @@ import json
 import sqlite3
 import secrets
 import time
+from secrets import compare_digest
 
 
 class Database:
-    def __init__(self, db_path):
+    def __init__(self, db_path: str):
         self.connection = sqlite3.connect(db_path, check_same_thread=False)
         self.cursor = self.connection.cursor()
 
@@ -15,7 +16,7 @@ class Database:
         print(password)
         return password
 
-    def add_user(self, username, email, password):
+    def add_user(self, username: str, email: str, password):
         # get highest id from the Database
         self.cursor.execute("SELECT MAX(id) FROM users")
         self.connection.commit()
@@ -29,7 +30,7 @@ class Database:
                             "?)", (username, f'{username}#{id}', email, password))
         self.connection.commit()
 
-    def generate_api_key(self, id):
+    def generate_api_key(self, id: int):
         # generate api token
         api_key = secrets.token_urlsafe(64)
         # get linux timestamp
@@ -41,12 +42,12 @@ class Database:
         self.connection.commit()
         return api_key
 
-    def get_api_key(self, id):
+    def get_api_key(self, id: int):
         self.cursor.execute("SELECT api_key FROM users WHERE id = ?", (id,))
         api_key = self.cursor.fetchone()[0][1]
         return api_key
 
-    def check_api_key(self, id):
+    def check_api_key(self, id: int):
         self.cursor.execute("SELECT api_key FROM users WHERE id = ?", (id,))
         timestamp, api_key = self.cursor.fetchone()[0]
 
@@ -63,6 +64,83 @@ class Database:
             entry[5] = json.loads(entry[5])
             newdata.append(entry)
         return newdata
+
+    def get_contacts(self, id: int):
+        self.cursor.execute("SELECT contacts FROM users WHERE id = ?", (id,))
+        contacts = self.cursor.fetchone()[0]
+        if contacts is None:
+            return []
+        else:
+            return json.loads(contacts)
+
+    def add_contact(self, id: int, contact_id: int):
+        self.cursor.execute("SELECT contacts FROM users WHERE id = ?", (id,))
+        contacts = self.cursor.fetchone()[0]
+        if contacts is None:
+            contacts = []
+        else:
+            contacts = json.loads(contacts)
+        contacts.append(contact_id)
+        contacts = list(set(contacts))
+        self.cursor.execute("UPDATE users SET contacts = ? WHERE id = ?",
+                            (json.dumps(contacts), id))
+        self.connection.commit()
+
+    def remove_contact(self, id: int, contact_id: int):
+        self.cursor.execute("SELECT contacts FROM users WHERE id = ?", (id,))
+        contacts = self.cursor.fetchone()[0]
+        if contacts is None:
+            contacts = []
+        else:
+            contacts = json.loads(contacts)
+        contacts.remove(contact_id)
+        self.cursor.execute("UPDATE users SET contacts = ? WHERE id = ?",
+                            (json.dumps(contacts), id))
+        self.connection.commit()
+
+    def get_messages(self, id: int, other_id: int = None):
+        if other_id is None:
+            self.cursor.execute("SELECT * FROM messages WHERE ids LIKE ?", (f'%{id}%',))
+            ids = self.cursor.fetchall()
+            newdata = list()
+            for entry in ids:
+                entry = list(entry)
+                entry[0] = json.loads(entry[0])
+                newdata.append(entry)
+            return newdata
+        else:
+            self.cursor.execute("SELECT * FROM messages WHERE ids LIKE ?", (f'%{id}%',))
+            ids = self.cursor.fetchall()
+            newdata = list()
+            for entry in ids:
+                entry = list(entry)
+                entry[0] = json.loads(entry[0])
+                if other_id in entry[0]:
+                    newdata.append(entry)
+            return newdata
+
+    def add_message(self, ids: list[int], message: str):
+        # get linux timestamp
+        timestamp = int(time.time())
+
+        ids_json = json.dumps(ids)
+
+        # make message not harmful to the database
+        message = message.replace("'", "''")
+
+        self.cursor.execute("INSERT INTO messages ( ids, message, timestamp) VALUES (?, ?, ?)",
+                            (ids_json, message, timestamp))
+        self.connection.commit()
+
+    def change_password(self, id: int, password: str, new_password_hash: str):
+        self.cursor.execute("SELECT password FROM users WHERE id = ?", (id,))
+        old_password = self.cursor.fetchone()[0]
+        if compare_digest(old_password, password):
+            self.cursor.execute("UPDATE users SET password = ? WHERE id = ?", (new_password_hash,
+                                                                               id))
+            self.connection.commit()
+            return True
+        return new_password_hash
 
     def close(self):
         self.connection.close()
